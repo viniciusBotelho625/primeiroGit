ab39e6d2ce451927f0fd648f029aea969bbae954
Lucas <lucasliet@github.com>
2020-03-07 17:42:16 -0300

    Construtor com this

--- Aluno.java
+++ Aluno.java
@@ -2,6 +2,11 @@ public class Aluno{
    private String nome;
    private int id;
    
+   public Aluno(String nome, int id){
+      this.nome = nome;
+      this.id   = id;
+   }
+
    public int getId(){
       return id;
    }
